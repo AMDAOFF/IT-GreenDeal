f4515ecd14a618e3c41ce06a65cbc08f6c1e73c7
Jonas Fuhlendorff <jonas.fuhlendorff@bitzerdk.com>
2021-09-20 11:18:25 +0200

    Project init

---
+++ Src/EnergiIOT/Energi.device/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Energi.device.cpp
+ *
+ * Created: 20-09-2021 11:17:11
+ * Author : jpf
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

954816da720539b16a4608c1d2660766d99d3d81
Kristian B. Kuhrt <Kristian.y8@gmail.com>
2021-09-23 00:23:01 +0200

    Added allergy info modal for when the info icon is clicked on a menu item. The design files can be considered done for now

---
+++ Src/CanteenSystem/Canteen.Web/Models/Allergy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Canteen.Web.Models
+{
+    public class Allergy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
